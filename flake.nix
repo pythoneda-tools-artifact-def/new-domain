c7c953d20ff29522e0b2da5da0c40919c4572dab
rydnr <github@acm-sl.org>
2024-11-15 15:26:20 +0100

    Support for Python 3.12

--- flake.nix
+++ flake.nix
@@ -81,7 +81,7 @@
     stringtemplate3 = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
-      url = "github:rydnr/nix-flakes/0.1.17?dir=stringtemplate3";
+      url = "github:rydnr/nix-flakes/stringtemplate3-3.1b?dir=stringtemplate3";
     };
   };
   outputs = inputs:
